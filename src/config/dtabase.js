8e3d024ef4680f0d94975da374a2a6c07998bb0f
Luthfi Yafi Alfiansyah <luthfialghz@gmail.com>
2023-03-10 10:27:08 +0700

    replacing with original

--- src/config/dtabase.js
+++ src/config/dtabase.js
@@ -1,18 +1,20 @@
 // Require mysql
-require('dotenv').config();
-const Pool = require('pg').Pool;
-
+require("dotenv").config();
+const Pool = require("pg").Pool;
 
 // Create a connection
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    port: process.env.DB_PORT,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+// const pool = new Pool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     port: process.env.DB_PORT,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+// });
+
+const pool = new Sequelize(DB_URI, {
+  define: {
+    timestamps: false,
+  },
 });
 
 module.exports = pool;
-
-
-
